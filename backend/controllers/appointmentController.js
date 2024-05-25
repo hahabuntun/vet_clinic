47dec5227f8f588c3ed6d01b4d61d2b2a4292819
hahabuntun <hahabuntun@gmail.com>
2024-05-26 00:37:59 +0300

    still need to finish this page

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -2,21 +2,14 @@ const mongoose = require("mongoose");
 const path = require('path');
 
 const Worker = require("../models/worker.js");
-const Animal = require("../models/animal.js");
 const Service = require("../models/service.js");
 const Appointment = require("../models/appointment.js");
 
 
 
 module.exports.get_doctor_shedule = async (req, res) => {
-    var queryDate = "";
-    if(req.query.date != null){
-        queryDate = new Date(req.query.date);
-    }
-    else{
-        queryDate = new Date();
-    }
-    
+    var queryDate = new Date();
+
     var services = await Service.find();
     var doctors = await Worker.find({type: "doctor"});
     
@@ -49,14 +42,45 @@ module.exports.get_doctor_shedule = async (req, res) => {
     res.render(path.join('clinic_administation_views', 'doctor_schedule'), data);
 }
 
+
+module.exports.get_single_doctor_shedule = async(req, res) => {
+    try{
+        var queryDate = req.query.date;
+        console.log(queryDate);
+        queryDate = new Date(queryDate);
+        const appointments = await Appointment.find({
+            doctor_id: req.params.id,
+            appointment_time: { $gte: queryDate, $lt: new Date(queryDate.getTime() + 24 * 60 * 60 * 1000) }
+        });
+        
+        const appointment_promises = appointments.map(async (appointment) => {
+            const serv = await Service.find({_id: appointment.service_id});
+            const service_name = serv[0].name;
+            appointment.service_name = service_name;
+    
+            const hours = appointment.appointment_time.getHours().toString().padStart(2, '0');
+            const minutes = appointment.appointment_time.getMinutes().toString().padStart(2, '0');
+            const timeStr = `${hours}:${minutes}`;
+            appointment.time = timeStr;
+            return appointment;
+        })
+        const updated_appointments = await Promise.all(appointment_promises);
+        return res.status(200).json({ appointments: updated_appointments });
+    }catch(error){
+        console.log(error);
+        return res.status(400).json({ error: error });
+        
+    }
+    
+}
+
+
 module.exports.add_schedule_entry = async (req, res) => {
     try {
         const data = JSON.parse(JSON.stringify(req.body));
+        console.log(data);
         const { service_id, doctor_id, appointment_time, date } = data;
-        const currentDate = new Date();
-        const fullAppointmentTime = date ?
-            new Date(`${date}T${appointment_time}`) :
-            new Date(`${currentDate.toISOString().slice(0, 10)}T${appointment_time}`);
+        const fullAppointmentTime = new Date(date);
         const appointment = new Appointment({ service_id: service_id, doctor_id: doctor_id, appointment_time: fullAppointmentTime, confirmed: false});
         await appointment.save();
         res.status(201).json({ message: "success" });
--- backend/routes/receptionist.js
+++ backend/routes/receptionist.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { add_client, edit_client, get_all_clients, delete_client } = require("../controllers/clientController");
 const { get_all_client_pets, add_pet_to_client, edit_client_pet, delete_client_pet} = require("../controllers/petController");
-const {get_doctor_shedule, add_schedule_entry, change_schedule_entry, delete_schedule_entry} = require("../controllers/appointmentController")
+const {get_doctor_shedule, get_single_doctor_shedule, add_schedule_entry, change_schedule_entry, delete_schedule_entry} = require("../controllers/appointmentController")
 const { verify_receptionist_token } = require("../middleware/authMiddleware");
 
 
@@ -17,6 +17,7 @@ router.patch('/pets/:petId',  edit_client_pet)
 router.delete('/pets/:petId', delete_client_pet)
 
 router.get("/doctor_schedule", get_doctor_shedule);
+router.get("/doctor_schedule/:id", get_single_doctor_shedule);
 router.post("/doctor_schedule", add_schedule_entry);
 router.put("/doctor_schedule/:id", change_schedule_entry);
 router.delete("/doctor_schedule/:id", delete_schedule_entry);
